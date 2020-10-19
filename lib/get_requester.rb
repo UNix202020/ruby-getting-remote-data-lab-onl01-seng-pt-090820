ed6482ac5cb94a16ead57a20e725cf373bf455d2
T S <taneymail@gmail.com>
2020-10-19 23:25:36 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,5 +1,13 @@
 # Write your code here
+require 'open-uri'
+require 'net/http'
+require 'json'
+
+
 class GetRequester
   def initialize(url)
     @url = url
   end
+  
+  def get_response_body
+    uri = URI.parse
